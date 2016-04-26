d521f062c096de773037a7829c66961a7e136a0e
Anubisunpluged <wisarut.khm@spulive.net>
2016-04-26 17:44:39 +0700

    SET-ITEM-MENU
    Set the item menu

--- app/src/main/java/com/roomscrumxyz/anubissmile/alphabetecho/MainActivity.java
+++ app/src/main/java/com/roomscrumxyz/anubissmile/alphabetecho/MainActivity.java
@@ -17,7 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+
+        /**
+         * FLOATING ACTIOIN BUTTON MENU
+         * */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override

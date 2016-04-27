2dc620a83f828b900d5bcbe8a5baba4f1e070deb
Anubisunpluged <wisarut.khm@spulive.net>
2016-04-28 05:12:06 +0700

    CREATE STRING ARRAY
    - Create string array alphabet-TH

--- app/src/main/java/com/roomscrumxyz/anubissmile/alphabetecho/FirstPage.java
+++ app/src/main/java/com/roomscrumxyz/anubissmile/alphabetecho/FirstPage.java
@@ -1,7 +1,10 @@
 package com.roomscrumxyz.anubissmile.alphabetecho;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class FirstPage extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class FirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
+        /**
+         * UI OBJECT BINDING.
+         */
+        ImageButton listening_btn = (ImageButton) findViewById(R.id.listening_btn);
+        ImageButton speek_out_btn = (ImageButton) findViewById(R.id.speek_out_btn);
+
+        /**
+         * PERFORM UI ON EVENT.
+         */
+
+        //listening button
+        listening_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "ไปฟังการอ่าน...", Snackbar.LENGTH_SHORT)
+                        .setAction("Action",null).show();
+            }
+        });
+
+        //speek out button
+        speek_out_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "ไปฝึกการออกเสียงด้วยเกมส์กันเถอะ...", Snackbar.LENGTH_SHORT)
+                        .setAction("Action",null).show();
+            }
+        });
+
     }
 }

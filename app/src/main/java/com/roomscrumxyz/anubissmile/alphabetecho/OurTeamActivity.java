e52e4b88f4b424a52525fd66993eae4aaf97e9d8
Anubisunpluged <wisarut.khm@spulive.net>
2016-05-07 17:47:55 +0700

    CREATE-NEW-ACTIVITY
    - Create new activit OurTeamActivity.java & activity_our_team.xml

---
+++ app/src/main/java/com/roomscrumxyz/anubissmile/alphabetecho/OurTeamActivity.java
@@ -0,0 +1,13 @@
+package com.roomscrumxyz.anubissmile.alphabetecho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OurTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_team);
+    }
+}
